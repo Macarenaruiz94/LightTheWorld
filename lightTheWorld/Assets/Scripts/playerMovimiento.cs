f51ab22fbdaf690ad4fa8186716f80365ea9734c
Macarena Ruiz <contactomruiz@yahoo.com>
2022-07-25 21:06:01 -0300

    idle player

--- lightTheWorld/Assets/Scripts/playerMovimiento.cs
+++ lightTheWorld/Assets/Scripts/playerMovimiento.cs
@@ -1,18 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerMovimiento : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    public float jump;
+    private int Health = 5;
+    public string sceneName;
+    Rigidbody2D rb;
+    public Animator animator;
+    private bool mirandoDerecha = true;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+
+    private void Movement()
+    {
+        var movimiento = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movimiento, 0, 0) * Time.deltaTime * speed;
+
+        //animator.SetFloat("speed", Mathf.Abs(movimiento));
+
+        if (movimiento > 0 && !mirandoDerecha)
+        {
+            Girar();
+        }
+        else if (movimiento < 0 && mirandoDerecha)
+        {
+            Girar();
+        }
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            Jump();
+        }
+    }
+    private void Girar()
+    {
+        mirandoDerecha = !mirandoDerecha;
+        Vector3 escala = transform.localScale;
+        escala.x *= -1;
+        transform.localScale = escala;
+    }
+
+    private void Jump()
+    {
+        if (Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            rb.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemigo")
+        {
+            Health -= 1;
+
+            if (Health == 0)
+            {
+                Destroy(gameObject);
+                SceneManager.LoadScene(sceneName);
+            }
+        }
     }
 }
